ae0837365dfb5bacff80faac3d82de509d37a0fa
lym4986 <114206198+lym4986@users.noreply.github.com>
2022-09-26 17:37:46 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -90,39 +90,39 @@ export const USER_CONFIG = {
         {"keyword": "love_day", date: "2022-07-22"},
       
       ]
-//     }, {
-//       // 想要发送的人的名字
-//       name: "老婆",
-//       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-//       id: "ouviY1Hxc6okBU_M-gaBoi0UvWvs",
-//       // 你想对他发送的模板消息的模板ID
-//       useTemplateId: "v1OAqIPHmPV0SRpdVB5e7xgXYS069KEKt7YeOKKN4CU",
-//       // 所在省份
-//       province: "河北",
-//       // 所在城市
-//       city: "石家庄",
-//       // 新历生日, 仅用作获取星座运势, 格式必须
-//       horoscopeDate: '1-30',
-//       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
-//       horoscopeDateType: '今日',
-//       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-//       openUrl: "https://wangxinleo.cn",
-//       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
-//       festivals: [
-//         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-// //         {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
-// //         {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
-//         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-//         {"type": "生日", "name": "老婆", "year": "1995", "date": "01-30"},
+    }, {
+      // 想要发送的人的名字
+      name: "老婆",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: "ouviY1EQl26belLdPrM23_sylJL8",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "v1OAqIPHmPV0SRpdVB5e7xgXYS069KEKt7YeOKKN4CU",
+      // 所在省份
+      province: "河北",
+      // 所在城市
+      city: "石家庄",
+      // 新历生日, 仅用作获取星座运势, 格式必须
+      horoscopeDate: '1-30',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "https://wangxinleo.cn",
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+//         {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
+//         {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {"type": "生日", "name": "老婆", "year": "1995", "date": "01-30"},
      
-//       ],
-//       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
-//       customizedDateList: [
-//         // 在一起的日子
-//         {"keyword": "love_day", date: "2022-07-22"},
+      ],
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      customizedDateList: [
+        // 在一起的日子
+        {"keyword": "love_day", date: "2022-07-22"},
       
-//       ]
-//     }
+      ]
+    }
     // 你可以不断按格式往下增加
     // ...
   ],
