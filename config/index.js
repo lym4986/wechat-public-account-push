9b2314c3301c528b002594b14ea29763c25d13e4
lym4986 <114206198+lym4986@users.noreply.github.com>
2022-09-26 17:43:09 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ouviY1EQl26belLdPrM23_sylJL8",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "1_RPCwoOzb4b8Tt64o6vkZwJnq8-pKI5B8tNMqHY3y8",
+      useTemplateId: "h_qa0TmvJevdOzQitf2Hh3h4UB5gFAIU8LrK_UNyZhg",
       // 所在省份
       province: "河北",
       // 所在城市
