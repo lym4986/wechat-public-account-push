da0554c24af1139618e32c8160af42fe668db0bf
lym4986 <114206198+lym4986@users.noreply.github.com>
2022-10-28 09:40:39 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -81,7 +81,7 @@ export const USER_CONFIG = {
 //         {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
 //         {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {"type": "*生日", "name": "老婆", "year": "1995", "date": "12-30"},
+        {"type": "生日", "name": "老婆", "year": "1995", "date": "1-21"},
      
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
@@ -113,7 +113,7 @@ export const USER_CONFIG = {
 //         {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
 //         {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {"type": "*生日", "name": "老婆", "year": "1995", "date": "12-30"},
+        {"type": "生日", "name": "老婆", "year": "1995", "date": "1-21"},
      
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
